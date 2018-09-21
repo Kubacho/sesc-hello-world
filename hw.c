3849642150ead89ffec6e16f14b9de877d8e5c74
Kubacho <kubacho.md@gmail.com>
2018-09-21 22:02:37 +0700

    11

--- hw.c
+++ hw.c
@@ -4,9 +4,9 @@ int main()
 {
 	char s[30];
 	scanf("%s", s);
-	//if(s[0] == '\0')
-	//	printf("Hello, nameless!\n");
-	//else
+	if(s[0] == '\0')
+		printf("Hello, nameless!\n");
+	else
 		printf("Hello, %s!\n", s);
 	return 0;
 }
