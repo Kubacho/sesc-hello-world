cadbbf412a93b5cc7f873f39831f7f5afd75bd3b
Kubacho <kubacho.md@gmail.com>
2018-09-21 20:38:27 +0700

    init

---
+++ hw.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	char s[30];
+	scanf("%s", s);
+	if(s[0] == '\0')
+		printf("Hello, nameless!\n", s);
+	else
+		printf("Hello, %s!\n", s);
+	return 0;
+}
---
+++ makefile
@@ -0,0 +1,7 @@
+run: hw
+    ./hw
+
+hw: hw.c
+    gcc -Wall -std=c11 hw.c -o hw
+
+build: hw
